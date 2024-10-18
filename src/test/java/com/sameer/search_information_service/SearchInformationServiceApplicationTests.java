e12bf239b5f1f8260c5cb22879ff545ffd5bd8b1
Fnu Sameer <sameersah7365@gmail.com>
2024-10-18 16:00:58 -0700

    initial commit

---
+++ src/test/java/com/sameer/search_information_service/SearchInformationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sameer.search_information_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchInformationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
